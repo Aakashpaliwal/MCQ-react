04682a7be562ad3d0b012383c72bf2e1f0934f33
Neo404 <aakashpaliwal95@gmail.com>
2018-09-26 01:25:49 -0700

    fix footer and delete some pages

--- src/App.js
+++ src/App.js
@@ -7,16 +7,6 @@ import {Route,Link} from  'react-router-dom'
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
-import Hvac from './Components/HVAC/Hvac';
-import Electrical from './Components/Electrical/Electrical';
-import Firefighting from './Components/Firefighting/Firefighting';
-import Interiors from './Components/Interiors/Interiors';
-import BMS from './Components/BMS/BMS';
-import PHE from './Components/PHE/PHE';
-import Projects from './Components/Projects/Projects';
-import About from './Components/About/About';
-import Career from './Components/Career/Career';
-import Blog from './Components/Blog/Blog';
 import UpperNavbar from './Components/UpperNavbar/UpperNavbar';
 import question2 from './Components/Home/question2';
 import question3 from './Components/Home/question3';
@@ -27,16 +17,6 @@ class App extends Component {
       <UpperNavbar />
        <Navbar />
        <Route path = "/" exact component = {Home} />
-        <Route path = "/Hvac" exact = {true} component = {Hvac} />
-        <Route path = "/Electrical" exact = {true} component = {Electrical} />
-        <Route path = "/Firefighting" exact = {true} component = {Firefighting} />
-        <Route path = "/Interiors" exact = {true} component = {Interiors} />
-        <Route path = "/BMS" exact = {true} component = {BMS} />
-        <Route path = "/PHE" exact = {true} component = {PHE} />
-        <Route path = "/Projects" exact = {true} component = {Projects} />
-        <Route path = "/About" exact = {true} component = {About} />
-        <Route path = "/Career" exact = {true} component = {Career} />
-        <Route path = "/Blog" exact = {true} component = {Blog} />
         <Route path = "/question2" exact = {true} component = {question2}/>
         <Route path = "/question3" exact = {true} component = {question3}/>
       <Footer />
